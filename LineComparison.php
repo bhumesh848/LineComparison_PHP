ef862294e0d04148741e127f87a1ff54023dff22
unknown <bhumesh.ranjaneb598@gmail.com>
2022-05-27 15:36:01 +0530

    Added:UC4 to compare lengths

--- LineComparison.php
+++ LineComparison.php
@@ -12,7 +12,7 @@
      public $x2;
      public $y1;
      public $y2;
-
+     
      function welcomeMessage(){
          echo "Welcome to line Comparison Computation Problem\n";
      }
@@ -52,13 +52,29 @@
             echo "\nLength2 is Greater than Length1\n";
         }
     }
-     }
+
+    function compareLengths($length1, $length2)
+    {
+        $length1 = (string) $length1;
+        $length2 = (string) $length2;
+        $compare = strcmp($length1, $length2);
+        if ($compare == 0) {
+            echo "\nBoth have Equal Length\n";
+        } elseif ($compare > 0) {
+            echo "\nLength1 is Greater than Length2\n";
+        } else {
+            echo "\nLength1 is Smaller than Length2\n";
+        }
+    }
+    }
      $lineComparison = new LineComparison();
      $lineComparison->welcomeMessage();
      $length1 = $lineComparison->calculateLength();
      echo "\nLength 1:" . $length1."\n";
      $length2 = $lineComparison->calculateLength();
      echo "\nLength 2:" . $length2;
-     $lineComparison->checkEquality($length1, $length2);
-    
+     //$lineComparison->checkEquality($length1, $length2);
+     $lineComparison->compareLengths($length1,$length2);
+
+     
 ?>
